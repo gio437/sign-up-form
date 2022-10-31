7c3155385bdd9a40ce3adb965bf7917acb0ce54f
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-10-30 20:25:21 -0400

    If applied, will add borders around text-boxes

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const button = document.querySelector(".account");
 button.addEventListener("click", check);
 
 function check() {
-    if (document.querySelector(".pass").value ==
-      document.querySelector('.confirm-pass').value) {
+    let check1 = document.querySelector(".pass");
+    let check2 = document.querySelector(".confirm-pass");
+
+    if (check1.value == check2.value) {
       document.getElementById('message').style.color = 'green';
       document.getElementById('message').innerHTML = 'matching';
+      check1.style.borderColor = 'green';
+      check2.style.borderColor = 'green';
+      check1.style.borderStyle = 'solid';
+      check2.style.borderStyle = 'solid';
     } else {
       document.getElementById('message').style.color = 'red';
       document.getElementById('message').innerHTML = 'not matching';
+      check1.style.borderColor = 'red';
+      check2.style.borderColor = 'red';
+      check1.style.borderStyle = 'solid';
+      check2.style.borderStyle = 'solid';
     }
   }
